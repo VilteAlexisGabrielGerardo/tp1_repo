0654662017d3deb63bfb40d2d21c67ee90eb6594
Alexis <Alexis@AnaMariaVilte>
2024-04-10 18:16:44 -0300

    ejercicio 2 completado

--- TP1_VilteAlexisGabriel/src/ar/edu/unju/fi/ejercicio2/Main.java
+++ TP1_VilteAlexisGabriel/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -3,11 +3,19 @@ package ar.edu.unju.fi.ejercicio2;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
-		
-	}
-	
+		String pais = "Argentina" ;
+		int edad = 20;
+		int altura = 2000;
+		double precio = 25.5;
+		long telefono = 388;
+		double numero = 0.5;
+        double coseno = Math.cos(numero);
+        System.out.println("El pais es: " + pais);
+        System.out.println("la edad es:" + edad);
+        System.out.println("La altura del edificio es: " + altura);
+        System.out.println("El precio de la cebolla es: " + precio);
+        System.out.println("El numero de telefono es " + telefono);
+        System.out.println("El coseno de 0.5 radianes es: " + coseno);
+}
 
 }
