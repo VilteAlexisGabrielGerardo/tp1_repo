0a7ef34c17584d9925fd36053df71e97d56fb661
Alexis <Alexis@AnaMariaVilte>
2024-04-15 19:29:29 -0300

    EJERCICIO 7 COMPLETADOO

---
+++ TP1_VilteAlexisGabriel/src/ar/edu/unju/fi/ejercicio7/main/Main.java
@@ -0,0 +1,25 @@
+package ar.edu.unju.fi.ejercicio7.main;
+import java.util.Scanner;
+import ar.edu.unju.fi.ejercicio7.model.*;
+public class Main {		
+		    public static void main(String[] args) {
+		        Scanner scanner = new Scanner(System.in);
+		        System.out.println("Ingrese el nombre del empleado:");
+		        String nombre = scanner.nextLine();
+		        System.out.println("Ingrese el legajo:");
+		        int legajo = Integer.parseInt(scanner.nextLine());
+		        System.out.println("Ingrese el salario:");
+		        double salario = Double.parseDouble(scanner.nextLine());
+		        Empleado empleado = new Empleado(nombre, legajo, salario);
+		        System.out.println("Datos del empleado antes del aumento:");
+		        empleado.mostrarDatos();
+		        empleado.darAumento();
+		        System.out.println("Datos del empleado después del aumento:");
+		        empleado.mostrarDatos();
+
+		        scanner.close();
+		    
+
+	}
+
+}
