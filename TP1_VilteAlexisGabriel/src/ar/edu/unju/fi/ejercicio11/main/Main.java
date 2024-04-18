cf59e4dbc2209e687442d2bb73117479d639e3e9
Alexis <Alexis@AnaMariaVilte>
2024-04-18 13:24:15 -0300

    Ejercicio 11 completado, tp1 continuacion

---
+++ TP1_VilteAlexisGabriel/src/ar/edu/unju/fi/ejercicio11/main/Main.java
@@ -0,0 +1,11 @@
+package ar.edu.unju.fi.ejercicio11.main;
+public class Main {
+
+	public static void main(String[] args) {
+				int j;
+				int i;
+		       for(j = 40 ,  i = 2 ;j>0; j--, i++) {
+		    	   System.out.println(j*i);
+		        }
+	}
+}
